c7b5e354334788cafb3064a53e613b1988b16e1d
Jeremy Stewart <jeremy.ste@gmail.com>
2019-07-04 14:58:47 +0000

    cleaning up

--- Network_Predictors/Latent_Predictor_FromAnalysis.py
+++ Network_Predictors/Latent_Predictor_FromAnalysis.py
@@ -2,13 +2,11 @@ import torch
 import torch.nn as nn
 
 class LatentPredictorFromAnalysis(nn.Module):
-    # def __init__(self, input_size, hidden_size=128,tsne_dimensions=3, n_clusters_out=10):
     def __init__(self, input_size, hidden_size=128, tsne_dimensions=3):
         super(LatentPredictorFromAnalysis, self).__init__()
         self.input_size = input_size
         self.hidden_size= hidden_size
         self.tsne_dimensions = tsne_dimensions
-        # self.n_clusters_out = n_clusters_out
 
         self.fc = nn.Linear(self.input_size, self.hidden_size)
         self.fc1 = nn.Linear(self.hidden_size, self.hidden_size)
@@ -19,15 +17,6 @@ class LatentPredictorFromAnalysis(nn.Module):
         self.fc2_2= nn.Linear(self.hidden_size, self.hidden_size)
         self.fc2_3= nn.Linear(self.hidden_size, self.hidden_size)
         self.fc2_4= nn.Linear(self.hidden_size, self.tsne_dimensions)
-
-        # self.fc3_1= nn.Linear(self.hidden_size, self.hidden_size)
-        # self.fc3_2= nn.Linear(self.hidden_size, self.hidden_size)
-        # self.fc3_3= nn.Linear(self.hidden_size, self.hidden_size)
-        # self.fc3_4= nn.Linear(self.hidden_size, self.hidden_size)
-        # self.fc3_5= nn.Linear(self.hidden_size, self.n_clusters_out)
-
-        # self.softmax = nn.Softmax(dim=1)
-
     def forward(self, inp, bsz=1):
         out = self.fc(inp.view(bsz, self.input_size))
         out = self.relu(out)
@@ -37,12 +26,4 @@ class LatentPredictorFromAnalysis(nn.Module):
         out2 = self.relu(self.fc2_2(out2))
         out2 = self.relu(self.fc2_3(out2))
         out2 = self.fc2_4(out2)
-
-        # out3 = self.relu(self.fc3_1(out))
-        # out3 = self.relu(self.fc3_2(out3))
-        # out3 = self.relu(self.fc3_3(out3))
-        # out3 = self.relu(self.fc3_4(out3))
-        # out3 = self.softmax(self.fc3_5(out3))
-
-        # return out2, out3
         return out2
--- Network_Predictors/train.py
+++ Network_Predictors/train.py
@@ -3,7 +3,6 @@ import os
 
 import argparse 
 parser = argparse.ArgumentParser()
-# parser.add_argument("--n_clusters", type=int, default=200, help="Valid values: [2, 3, 4, 8, 12, 16, 20, 24, 30, 40, 60, 80, 120, 200]")
 parser.add_argument("--hidden_size", type=int, default=256)
 parser.add_argument("--lr", type=float, default=1e-5)
 parser.add_argument("--weight_decay", type=float, default=1e-8)
@@ -11,12 +10,8 @@ parser.add_argument("--n_epochs", type=int, default=1000)
 parser.add_argument("--batch_size", type=int, default=500)
 args = parser.parse_args()
 
-# n_clusters = args.n_clusters #set number of clusters here. must be valid in pre-constructed kmeans clustering data.
 hidden_size=args.hidden_size
 lr=args.lr
-
-# ClusteringJSONDatFile = os.path.join(os.path.dirname(__file__), "..", "Clustering", "JSON", ("COMBINES_TSNE_"+str(n_clusters)+".json"))
-# assert os.path.exists(ClusteringJSONDatFile), "[ERROR] Clustering Dat File doesn't exist: {}".format(ClusteringJSONDatFile)
 
 preppedFlattenedDataFilename= os.path.join(os.path.dirname(__file__), "..", "allDat_PreppedFlattened.json")
 
@@ -27,18 +22,7 @@ tsneEmbeddingsDataFilename = os.path.join(os.path.dirname(__file__), "..", "tSNE
 with open(tsneEmbeddingsDataFilename, "r") as f: tsneEmbeddings = json.load(f)['COMBINES_TSNE']
 print(len(tsneEmbeddings))
 
-# with open(ClusteringJSONDatFile, "r") as f: ClusterDat = json.load(f)    
-# assert ClusterDat['n_clusters']==n_clusters, "[ERROR] Invalid number of clusters in file: {} != {}".format(ClusterDat['n_clusters'], n_clusters)
-# ClusterIds = ClusterDat['out_predict']
-# print(len(ClusterIds))
-
 assert len(combinedSourceData)==len(tsneEmbeddings)
-# assert len(combinedSourceData)==len(ClusterIds)
-
-# combined = list(zip(combinedSourceData, tsneEmbeddings, ClusterIds))
-# from random import shuffle
-# shuffle(combined)
-# combinedSourceData[:], tsneEmbeddings[:], ClusterIds[:] = zip(*combined)
 
 combined = list(zip(combinedSourceData, tsneEmbeddings))
 from random import shuffle
@@ -58,7 +42,6 @@ print(m)
 optimizer = optim.Adam(m.parameters(), lr=lr, weight_decay=args.weight_decay)
 
 tsneCrit = nn.L1Loss()
-# clusterCrit = nn.NLLLoss()
 
 n_epochs = args.n_epochs
 batchsize=args.batch_size
@@ -71,7 +54,6 @@ print("Remainder: {}".format(remainder))
 valStartIdx = (numfullbatches-1)*batchsize
 validationInp =  combinedSourceData[valStartIdx:]
 validationTsne= tsneEmbeddings[valStartIdx:]
-# validationClusters= ClusterIds[valStartIdx:]
 
 allTrainingLosses = []
 allValidationLosses = []
@@ -82,18 +64,13 @@ for epoch in range(n_epochs):
     for b in range(numfullbatches-1):
         inp = combinedSourceData[b*batchsize:(b+1)*batchsize]
         tsneTar = tsneEmbeddings[b*batchsize:(b+1)*batchsize]
-        # clusterTar=ClusterIds[b*batchsize:(b+1)*batchsize]
 
         inp = torch.tensor(inp, dtype=torch.float).cuda()
         tsneTar=torch.tensor(tsneTar, dtype=torch.float).cuda()
-        # clusterTar=torch.tensor(clusterTar, dtype=torch.long).cuda()
-
-        # tsneOut, clusterOut = m(inp, batchsize)
         tsneOut = m(inp, batchsize)
 
         optimizer.zero_grad()
         loss = tsneCrit(tsneOut, tsneTar)
-        # loss += clusterCrit(clusterOut, clusterTar)
 
         loss.backward()
         optimizer.step()
@@ -104,13 +81,10 @@ for epoch in range(n_epochs):
     
     valInp = torch.tensor(validationInp, dtype=torch.float).cuda()
     valTsne= torch.tensor(validationTsne, dtype=torch.float).cuda()
-    # valCluster=torch.tensor(validationClusters, dtype=torch.long).cuda()
 
     m.eval()
-    # tsneOut, clusterOut = m(valInp, valInp.shape[0])
     tsneOut = m(valInp, valInp.shape[0])
     loss = tsneCrit(tsneOut, valTsne)
-    # loss += clusterCrit(clusterOut, valCluster)
     print("\t\tValidation Loss: {}".format(loss.item()/valInp.shape[0]))
     allValidationLosses.append(loss.item()/valInp.shape[0])
 
