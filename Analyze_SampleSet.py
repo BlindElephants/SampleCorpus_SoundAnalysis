40dc51b5550d384636d0201c8e820ae429ffd197
Jeremy Stewart <jeremy.ste@gmail.com>
2019-09-10 13:20:22 -0400

    added additional exception catches

--- Analyze_SampleSet.py
+++ Analyze_SampleSet.py
@@ -48,59 +48,67 @@ print("Found {} samples.".format(len(SamplesPaths)))
 SaveCheckpointEvery=100
 
 for idx, samples in enumerate(SamplesPaths):
+
+    if idx==8914: continue
+
     if samples[2] not in OutputAnalysisData['files parsed']:
         print("Analyzing sample {} of {}".format(idx, len(SamplesPaths)), end='')
 
-        y, sr=librosa.load(samples[0])
-        if y.shape[0]>0:
-            stft=np.abs(librosa.core.stft(y))
-            cqt =np.abs(librosa.core.cqt(y,sr=sr))
-            specCentroid=librosa.feature.spectral_centroid(y,sr=sr)
-            specBandwidth=librosa.feature.spectral_bandwidth(y,sr=sr)
-            specContrast =librosa.feature.spectral_contrast(y,sr=sr)
-            specFlatness =librosa.feature.spectral_flatness(y=y)
-            specRolloff  =librosa.feature.spectral_rolloff(y=y,sr=sr)
-            duration=librosa.core.get_duration(y,sr)
+        try:
+            y, sr=librosa.load(samples[0])
+            if y.shape[0]>0:
+                stft=np.abs(librosa.core.stft(y))
+                cqt =np.abs(librosa.core.cqt(y,sr=sr))
+                specCentroid=librosa.feature.spectral_centroid(y,sr=sr)
+                specBandwidth=librosa.feature.spectral_bandwidth(y,sr=sr)
+                specContrast =librosa.feature.spectral_contrast(y,sr=sr)
+                specFlatness =librosa.feature.spectral_flatness(y=y)
+                specRolloff  =librosa.feature.spectral_rolloff(y=y,sr=sr)
+                duration=librosa.core.get_duration(y,sr)
 
-            ThisSampleData={
-                'sample_filename': samples[2],
-                'sample_tidalName': samples[3],
-                'duration': duration,
-                'stft_m': np.mean(stft, axis=1).tolist(),
-                'stft_std': np.std(stft, axis=1).tolist(),
-                'stft_sum': np.sum(stft, axis=1).tolist(),
-                'cqt_m': np.mean(cqt, axis=1).tolist(),
-                'cqt_std': np.std(cqt, axis=1).tolist(),
-                'cqt_sum': np.sum(cqt, axis=1).tolist(),
-                'spec centroid_m': np.mean(specCentroid, axis=1).tolist(),
-                'spec centroid_std': np.std(specCentroid,axis=1).tolist(),
-                'spec centroid_sum': np.sum(specCentroid,axis=1).tolist(),
-                'spec bandwidth_m': np.mean(specBandwidth, axis=1).tolist(),
-                'spec bandwidth_std': np.std(specBandwidth,axis=1).tolist(),
-                'spec bandwidth_sum': np.sum(specBandwidth,axis=1).tolist(),
-                'spec contrast_m': np.mean(specContrast, axis=1).tolist(),
-                'spec contrast_std':np.std(specContrast, axis=1).tolist(),
-                'spec contrast_sum':np.sum(specContrast, axis=1).tolist(),
-                'spec flatness_m':np.mean(specFlatness, axis=1).tolist(),
-                'spec flatness_std':np.std(specFlatness,axis=1).tolist(),
-                'spec flatness_sum':np.sum(specFlatness,axis=1).tolist(),
-                'spec rolloff_m': np.mean(specRolloff, axis=1).tolist(),
-                'spec rolloff_std': np.std(specRolloff,axis=1).tolist(),
-                'spec rolloff_sum': np.sum(specRolloff,axis=1).tolist(),
-                'generated': str(datetime.datetime.now())
-            }
+                ThisSampleData={
+                    'sample_filename': samples[2],
+                    'sample_tidalName': samples[3],
+                    'duration': duration,
+                    'stft_m': np.mean(stft, axis=1).tolist(),
+                    'stft_std': np.std(stft, axis=1).tolist(),
+                    'stft_sum': np.sum(stft, axis=1).tolist(),
+                    'cqt_m': np.mean(cqt, axis=1).tolist(),
+                    'cqt_std': np.std(cqt, axis=1).tolist(),
+                    'cqt_sum': np.sum(cqt, axis=1).tolist(),
+                    'spec centroid_m': np.mean(specCentroid, axis=1).tolist(),
+                    'spec centroid_std': np.std(specCentroid,axis=1).tolist(),
+                    'spec centroid_sum': np.sum(specCentroid,axis=1).tolist(),
+                    'spec bandwidth_m': np.mean(specBandwidth, axis=1).tolist(),
+                    'spec bandwidth_std': np.std(specBandwidth,axis=1).tolist(),
+                    'spec bandwidth_sum': np.sum(specBandwidth,axis=1).tolist(),
+                    'spec contrast_m': np.mean(specContrast, axis=1).tolist(),
+                    'spec contrast_std':np.std(specContrast, axis=1).tolist(),
+                    'spec contrast_sum':np.sum(specContrast, axis=1).tolist(),
+                    'spec flatness_m':np.mean(specFlatness, axis=1).tolist(),
+                    'spec flatness_std':np.std(specFlatness,axis=1).tolist(),
+                    'spec flatness_sum':np.sum(specFlatness,axis=1).tolist(),
+                    'spec rolloff_m': np.mean(specRolloff, axis=1).tolist(),
+                    'spec rolloff_std': np.std(specRolloff,axis=1).tolist(),
+                    'spec rolloff_sum': np.sum(specRolloff,axis=1).tolist(),
+                    'generated': str(datetime.datetime.now())
+                }
 
-            with open(os.path.join(SampleAnalysisDataFolder, "Samples", samples[1]+".json"), "w") as f:
-                json.dump(ThisSampleData, f)
-            OutputAnalysisData['files parsed'].append(samples[2])
-            print("[DONE]")
+                with open(os.path.join(SampleAnalysisDataFolder, "Samples", samples[1]+".json"), "w") as f:
+                    json.dump(ThisSampleData, f)
+                OutputAnalysisData['files parsed'].append(samples[2])
+                print("[DONE]")
 
-            if (idx+1)%SaveCheckpointEvery==0:
-                print("Saving checkpoint.")
-                with open(OutputAnalysisFileName, "w") as f:
-                    json.dump(OutputAnalysisData, f)
-        else:
-            print("Abort.")
+                if (idx+1)%SaveCheckpointEvery==0:
+                    print("Saving checkpoint.")
+                    with open(OutputAnalysisFileName, "w") as f:
+                        json.dump(OutputAnalysisData, f)
+            else:
+                print("Abort.")
+        except ValueError:
+            print("[ValueError]")
+        except RuntimeError:
+            print("[RuntimeError]")
 
 OutputAnalysisData['finished']=True
 OutputAnalysisData['end time']=str(datetime.datetime.now())
